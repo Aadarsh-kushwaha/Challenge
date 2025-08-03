415ceaf76cd0cb12e807519c277b9cf273f81ecf
Aadarsh Kushwaha <aadarshkus555@gmail.com>
2025-08-03 18:55:12 +0530

    intersection

---
+++ Day2/Day3/intersectionofarrays.java
@@ -0,0 +1,26 @@
+package Day2.Day3;
+
+class intersectionofarrays {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        boolean[] visited = new boolean[nums2.length];
+        for(int i=0;i<nums1.length;i++){
+            for(int j = 0;j<nums2.length;j++){
+                if(nums1[i]==nums2[j] && !visited[j]){
+                    ans.add(nums1[i]);
+                    visited[j]=true;
+                    break ;
+                }
+            }
+        }
+      // Convert ArrayList to int[]
+        int[] result = new int[ans.size()];
+        for (int i = 0; i < ans.size(); i++) {
+            result[i] = ans.get(i);
+        }
+
+        return result;
+
+    }
+}
