1de83cbcd433add7e881f20c1e012df3bc578029
Aadarsh Kushwaha <aadarshkus555@gmail.com>
2025-08-03 18:57:33 +0530

    plusone

---
+++ Day2/Day3/plusone.java
@@ -0,0 +1,33 @@
+package Day2.Day3;
+
+
+
+class plusone {
+    public int[] plusOne(int[] digits) {
+        digits[digits.length - 1] += 1;
+
+        for (int i = digits.length - 1; i > 0; i--) {
+            if (digits[i] >= 10) {
+                digits[i] %= 10;
+                digits[i - 1] += 1;
+            } else {
+                // No carry needed, return early
+                return digits;
+            }
+        }
+
+        // Check if carry is needed at the highest digit
+        if (digits[0] >= 10) {
+            int[] result = new int[digits.length + 1];
+            result[0] = 1;
+            digits[0] %= 10;
+            for (int i = 0; i < digits.length; i++) {
+                result[i + 1] = digits[i];
+            }
+            return result;
+        }
+        
+
+        return digits;
+    }
+}
