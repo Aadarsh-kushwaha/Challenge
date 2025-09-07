6695a701156de2f00b1b635213bda44fcb41c4e6
Aadarsh Kushwaha <aadarshkus555@gmail.com>
2025-09-07 19:01:13 +0530

    Day34

---
+++ Day34/LinkedList.java
@@ -0,0 +1,40 @@
+public class LinkedList {
+    public static class Node{
+        int data ;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data){
+        Node newNode = new Node(data);//create new node
+        if(head==null){
+            head=tail=newNode;
+            return ;
+        }
+        newNode.next =head;
+        head = newNode;
+    }
+    public void addLast(int data){
+        Node newNode = new Node(data);//create new node
+         if(head==null){
+            head=tail=newNode;
+            return ;
+        }
+        tail.next=newNode;
+        tail = newNode;
+
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        
+
+    }
+}
