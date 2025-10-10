24c7d0486a318c833c1834d43939b489be513b89
Aadarsh Kushwaha <aadarshkus555@gmail.com>
2025-10-11 01:47:02 +0530

    remove element when dup

---
+++ Day47/REmoveContDuplicate.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String removeDuplicates(String str) {
+        Stack<Character> s = new Stack<>();
+         for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            
+            // If stack not empty and top is same → remove duplicate
+            if (!s.isEmpty() && s.peek() == ch) {
+                s.pop();
+            } else {
+                s.push(ch);
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        while(!s.isEmpty()){
+            sb.append(s.pop());
+        }
+       
+
+         sb.reverse();
+         return sb.toString();
+
+    }
+}
