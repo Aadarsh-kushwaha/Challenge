03a7dffca5854b90a0833f34dd99fe299a2d7033
Aadarsh Kushwaha <aadarshkus555@gmail.com>
2025-10-21 12:57:59 +0530

    KeyPRessed

---
+++ Day51/keysPressed.java
@@ -0,0 +1,21 @@
+
+https://leetcode.com/problems/slowest-key/description/
+
+
+class Solution {
+    public char slowestKey(int[] releaseTimes, String keysPressed) {
+      int maxDuration = releaseTimes[0];
+      char slowest  = keysPressed.charAt(0);
+      for(int i=1;i<releaseTimes.length;i++){
+        int curr = releaseTimes[i]-releaseTimes[i-1];
+        if(curr>maxDuration || curr==maxDuration && keysPressed.charAt(i)>slowest){
+            maxDuration = curr;
+            slowest =  keysPressed.charAt(i);
+        }
+      }
+
+      return slowest;
+    }
+}
+
+
